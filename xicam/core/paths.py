7875cd94669995208304c295c26dbee38e55e568
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-07-02 10:01:20 -0700

    Add user_dev_dir path to plugin dirs

--- xicam/core/paths.py
+++ xicam/core/paths.py
@@ -1,16 +1,25 @@
 from appdirs import user_config_dir, site_config_dir, user_cache_dir
 import os
+import platform
 
 user_cache_dir = user_cache_dir(appname='xicam')
 site_config_dir = site_config_dir(appname='xicam')
 user_config_dir = user_config_dir(appname='xicam')
+user_dev_dir = os.path.expanduser('~/Xi-cam/plugins')
+op_sys = platform.system()
+if op_sys == 'Darwin':  # User config dir incompatible with venv on darwin (space in path name conflicts)
+    user_plugin_dir = os.path.join(user_cache_dir(appname='xicam'), 'plugins')
+else:
+    user_plugin_dir = os.path.join(user_config_dir(appname='xicam'), 'plugins')
+site_plugin_dir = os.path.join(site_config_dir(appname='xicam'), 'plugins')
 
-try:
-    os.makedirs(user_cache_dir)
-except FileExistsError:
-    pass
 
-try:
-    os.makedirs(user_config_dir)
-except FileExistsError:
-    pass
+def init_dir(path):
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
+
+
+for path in [user_cache_dir, user_config_dir, user_plugin_dir]:
+    init_dir(path)
