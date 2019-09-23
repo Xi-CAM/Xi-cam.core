af9df516924997b0db1fefa82c66da67b6b3c8c6
Dylan McReynolds <dmcreynolds@lbl.gov>
2019-09-23 13:31:57 -0700

    Migrate from intake_bluesy to databroker v1.x

--- xicam/core/data/catalogs/JSONLCatalog.py
+++ xicam/core/data/catalogs/JSONLCatalog.py
@@ -1,11 +1,20 @@
-from intake_bluesky.jsonl import BlueskyJSONLCatalog
+# from intake_bluesky.jsonl import BlueskyJSONLCatalog
 from xicam.plugins import CatalogPlugin
 from qtpy.QtWidgets import QFileDialog
 import os
 import glob
+from pkg_resources import iter_entry_points
 
+jsonCatalogType = None
+for entry_point in iter_entry_points('intake.drivers'):
+    if entry_point.name == 'bluesky-jsonl-catalog':
+        jsonCatalogType = entry_point.load()
+        break
 
-class JSONLCatalogPlugin(BlueskyJSONLCatalog, CatalogPlugin):
+else:
+    raise ImportError('JSONLCatalog plugin unable to find JSONLCatalog type. Check databroker configuration.')
+
+class JSONLCatalogPlugin(jsonCatalogType, CatalogPlugin):
     name = "JSONL"
 
     def __init__(self):
