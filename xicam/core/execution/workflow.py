9dfdc77d41a50d4acdf6835cb23caeb6fea46454
Ronald Pandolfi <ronpandolfi@gmail.com>
2020-01-08 16:19:57 -0800

    Move links to Workflow context

--- xicam/core/execution/workflow.py
+++ xicam/core/execution/workflow.py
@@ -1,13 +1,191 @@
 from xicam.plugins import ProcessingPlugin, OperationPlugin
-from typing import Callable, List, Union
-from collections import OrderedDict
+from typing import Callable, List, Union, Tuple
+from collections import defaultdict
 from xicam.core import msg, execution
 from xicam.core.threads import QThreadFuture, QThreadFutureIterator
+from weakref import ref
+
 
 # TODO: add debug flag that checks mutations by hashing inputs
 
+class Graph(object):
+    def __init__(self):
+        self._operations = []
+        self._inbound_links = defaultdict(lambda: defaultdict(lambda: []))
+        self._outbound_links = defaultdict(lambda: defaultdict(lambda: []))
 
-class WorkflowProcess:
+    def add_operation(self, operation: OperationPlugin):
+        self.add_operations([operation])
+
+    def add_operations(self, operations: List[OperationPlugin]):
+        for operation in operations:
+            self.insert_operation(len(self._operations) + 1, operation)
+
+    def insert_operation(self, index: int, operation: OperationPlugin):
+        self._operations.insert(index, operation)
+        operation._workflow = ref(self)
+
+        self.notify()
+
+    def add_link(self, source, dest, source_param, dest_param):
+        if source_param not in source.output_names:
+            raise NameError(
+                f"An output named \"{source_param}\" could not be found in the sender operation, {source.name}")
+        elif dest_param not in dest.input_names:
+            raise NameError(
+                f"An input named \"{dest_param}\" could not be found in the receiver operation, {dest.name}")
+        self._inbound_links[dest][source].append((source_param, dest_param))
+        self._outbound_links[source][dest].append((source_param, dest_param))
+
+        self.notify()
+
+    def remove_link(self, source, dest, source_param, dest_param):
+        self._inbound_links[dest][source].remove((source_param, dest_param))
+        self._outbound_links[source][dest].remove((source_param, dest_param))
+
+        self.notify()
+
+    def get_inbound_links(self, operation):
+        return self._inbound_links[operation]
+
+    def get_outbound_links(self, operation):
+        return self._outbound_links[operation]
+
+    def clear_operation_links(self, operation):
+        for links in self.get_inbound_links(operation):
+            del links
+        for links in self.get_outbound_links(operation):
+            del links
+
+        self.notify()
+
+    def clear_links(self):
+        self._inbound_links.clear()
+        self._outbound_links.clear()
+
+        self.notify()
+
+    def clear_operations(self):
+        self._operations.clear()
+        self._inbound_links.clear()
+        self._outbound_links.clear()
+
+        self.notify()
+
+    def remove_operation(self, operation, remove_orphan_links=True):
+        self._operations.remove(operation)
+
+        if remove_orphan_links:
+            self.clear_operation_links(operation)
+        else:
+            return self.get_inbound_links(operation), self.get_outbound_links(operation)
+
+        self.notify()
+
+    def _end_operations(self):
+        """
+        find tasks at the end of the graph and work up
+        check inputs and remove dependency nodes, what is left is unique ones
+        """
+
+        end_tasks = set(self.operations) - self._outbound_links.keys()
+
+        msg.logMessage("End tasks:", *[task.name for task in end_tasks], msg.DEBUG)
+        return end_tasks
+
+    def _dask_graph(self):
+        dask_graph = {}
+
+        for operation in self.operations:
+            links = {}
+            dependent_ids = []
+            for dep_operation, inbound_links in self._inbound_links[operation].items():
+                for (source_param, dest_param) in inbound_links:
+                    links[dest_param] = source_param
+                dependent_ids.append(dep_operation.id)
+
+            node = _OperationWrapper(operation, links)
+            dask_graph[operation.id] = (node, *dependent_ids)
+
+        return dask_graph
+
+    def as_dask_graph(self):
+        """
+        process from end tasks and into all dependent ones
+        """
+
+        for (i, node) in enumerate(self.operations):
+            node.id = str(i)
+
+        end_tasks = self._end_operations()
+
+        dask_graph = self._dask_graph()
+        end_task_ids = [i.id for i in end_tasks]
+
+        return dask_graph, end_task_ids
+
+    @property
+    def operations(self):
+        return self._operations
+
+    def links(self):
+        return [(src, dest, src_param, dest_param) for dest, links in self._outbound_links.items() for
+                src, (src_param, dest_param) in links.items()]
+
+    def operation_links(self, operation):
+        return [(operation, dest, src_param, dest_param) for dest, (src_param, dest_param) in
+                self._outbound_links[operation]]
+
+    def set_disabled(self, operation: OperationPlugin, value: bool = True, remove_orphan_links=True):
+        operation.disabled = value
+        orphaned_links = []
+        if value:
+            if remove_orphan_links:
+                self.clear_operation_links(operation)
+            else:
+                orphaned_links = self.operation_links(operation)
+
+        self.notify()
+        return orphaned_links
+
+    def auto_connect_all(self):
+        self.clear_links()
+
+        # for each operation
+        for input_operation in self.operations:
+
+            # for each input of given operation
+            for input_name in input_operation.input_names():
+                bestmatch = None
+                matchness = 0
+                # Parse backwards from the given operation, looking for matching outputs
+                for output_operation in reversed(self.operations[: self.operations.index(input_operation)]):
+                    # check each output
+                    for output_name in output_operation.output_names:
+                        # if matching name
+                        if output_name == input_name:
+                            # if a name match hasn't been found
+                            if matchness < 1:
+                                bestmatch = output_operation, output_name
+                                matchness = 1
+                                # if a name+type match hasn't been found
+                                if output_operation.output_types[output_name] == input_operation.input_types[
+                                    input_name]:
+                                    if matchness < 2:
+                                        bestmatch = output_operation, output_name
+                                        matchness = 2
+                if bestmatch:
+                    self.add_link(bestmatch[0], input_operation, bestmatch[1], input_name)
+                    msg.logMessage(
+                        f"connected {bestmatch[0].name}:{bestmatch[1]} to {input_operation.name}:{input_name}",
+                        level=msg.DEBUG,
+                    )
+
+    def notify(self, *args, **kwargs):
+        ...
+
+
+class _OperationWrapper:
     def __init__(self, node, named_args, islocal=False):
         self.node = node
         self.named_args = named_args
@@ -34,178 +212,21 @@ class WorkflowProcess:
         return self.node.__class__.__name__
 
 
-class Workflow(object):
+class Workflow(Graph):
     def __init__(self, name="", operations=None):
-        self._operations = []  # type: List[OperationPlugin]
+        super(Workflow, self).__init__()
+        # self._operations = []  # type: List[OperationPlugin]
         self._observers = set()
+        # self._links = []  # type: List[Tuple[ref, str, ref, str]]
         if name:
             self.name = name
 
         if operations:
-            self._operations.extend(operations)
+            # self._operations.extend(operations)
+            self.add_operations(operations)
         self.staged = False
 
         self.lastresult = []
-
-    def findEndTasks(self):
-        """
-        find tasks at the end of the graph and work up
-        check inputs and remove dependency nodes, what is left is unique ones
-        """
-
-        dependent_tasks = set()
-
-        for operation in self.operations:
-            for weak_ref_operation, _, __ in operation._inbound_links:
-                dependent_tasks.add(weak_ref_operation())
-
-        end_tasks = set(self.operations) - dependent_tasks
-
-        msg.logMessage("End tasks:", *[task.name for task in end_tasks], msg.DEBUG)
-        return end_tasks
-
-    def generateGraph(self):
-        """
-        Recursive function that adds
-        :param dsk:
-        :param q:
-        :param operation:
-        :param mapped_node:
-        :return:
-        """
-
-        dask_graph = {}
-
-        for operation in self.operations:
-            links = {}
-            dependent_ids = []
-            for weak_op_ref, output_name, input_name in operation._inbound_links:
-                links[input_name] = output_name
-                dependent_ids.append(weak_op_ref().id)
-            node = WorkflowProcess(operation, links)
-
-            dask_graph[operation.id] = (node, *dependent_ids)
-
-        return dask_graph
-
-    def convertGraph(self):
-        """
-        process from end tasks and into all dependent ones
-        """
-
-        for (i, node) in enumerate(self.operations):
-            node.id = str(i)
-
-        end_tasks = self.findEndTasks()
-
-        dask_graph = self.generateGraph()
-        end_task_ids = [i.id for i in end_tasks]
-
-        return dask_graph, end_task_ids
-
-    def addOperation(self, operation: Union[OperationPlugin, callable], autoconnectall: bool = False):
-        """
-        Adds a Operation as a child.
-        Parameters
-        ----------
-        operation:    OperationPlugin
-            Operation to add
-        autowireup: bool
-            If True, connects Outputs of the previously added Operation to the Inputs of operation, matching names and types
-        """
-        if not isinstance(operation, OperationPlugin) and callable(operation):
-            operation = OperationPlugin(operation)
-
-        self._operations.append(operation)
-        operation._workflow = self
-        if autoconnectall:
-            self.autoConnectAll()
-        self.update()
-        return operation
-
-    def insertOperation(self, index: int, operation: OperationPlugin, autoconnectall: bool = False):
-        self._operations.insert(index, operation)
-        operation._workflow = self
-        self.update()
-        if autoconnectall:
-            self.autoConnectAll()
-
-    def removeOperation(self, operation: OperationPlugin = None, index=None, autoconnectall=False):
-        # Note: It is not necessary to disconnect operations, since they operate with weakrefs; it is done here for completeness
-        for possible_linked_operation in self.operations:
-            for link in possible_linked_operation._inbound_links:
-                if link[0]() == operation:
-                    possible_linked_operation._inbound_links.remove(link)
-
-        operation._inbound_links.clear()
-
-        if not operation:
-            operation = self._operations[index]
-        self._operations.remove(operation)
-        # operation.detach()  # this wasn't doing anything; what was it for?
-        operation._workflow = None
-        if autoconnectall:
-            self.autoConnectAll()
-        self.update()
-
-    def autoConnectAll(self):
-        self.clearConnections()
-
-        # for each operation
-        for input_operation in self.operations:
-
-            # for each input of given operation
-            for input_name in input_operation.input_names():
-                bestmatch = None
-                matchness = 0
-                # Parse backwards from the given operation, looking for matching outputs
-                for output_operation in reversed(self.operations[: self.operations.index(input_operation)]):
-                    # check each output
-                    for output_name in output_operation.output_names:
-                        # if matching name
-                        if output_name == input_name:
-                            # if a name match hasn't been found
-                            if matchness < 1:
-                                bestmatch = output_operation, output_name
-                                matchness = 1
-                                # if a name+type match hasn't been found
-                                if output_operation.output_types[output_name] == input_operation.input_types[
-                                    input_name]:
-                                    if matchness < 2:
-                                        bestmatch = output_operation, output_name
-                                        matchness = 2
-                if bestmatch:
-                    bestmatch[0].link(bestmatch[0], input_name, bestmatch[1])
-                    msg.logMessage(
-                        f"connected {bestmatch[0].name}:{bestmatch[1]} to {input_operation.name}:{input_name}",
-                        level=msg.DEBUG,
-                    )
-
-    def clearConnections(self):
-        for operation in self.operations:
-            operation._inbound_links.clear()
-
-    def setDisabled(self, operation: OperationPlugin, value: bool = True, autoconnectall: bool = False):
-        operation.disabled = value
-        if value:
-            operation._inbound_links.clear()
-        if autoconnectall:
-            self.autoConnectAll()
-        self.update()
-
-    @property
-    def operations(self) -> List[OperationPlugin]:
-        return [operation for operation in self._operations if not operation.disabled]
-
-    @operations.setter
-    def operations(self, operations):
-        for operation in self._operations:
-            operation._workflow = None
-
-        self._operations = operations
-        for operation in operations:
-            operation._workflow = self
-        self.update()
 
     def stage(self, connection):
         """
@@ -223,17 +244,17 @@ class Workflow(object):
         # TODO: use cam-link to mirror installation of plugin packages
 
     def execute(
-        self,
+            self,
             executor=None,
-        connection=None,
-        callback_slot=None,
-        finished_slot=None,
-        except_slot=None,
-        default_exhandle=True,
-        lock=None,
-        fill_kwargs=True,
-        threadkey=None,
-        **kwargs,
+            connection=None,
+            callback_slot=None,
+            finished_slot=None,
+            except_slot=None,
+            default_exhandle=True,
+            lock=None,
+            fill_kwargs=True,
+            threadkey=None,
+            **kwargs,
     ):
         """
         Execute this workflow on the specified host. Connection will be a Connection object (WIP) keeping a connection
@@ -278,19 +299,18 @@ class Workflow(object):
 
         return executor.execute(self)
 
-
     def execute_all(
-        self,
-        connection,
+            self,
+            connection,
             executor=None,
-        callback_slot=None,
-        finished_slot=None,
-        except_slot=None,
-        default_exhandle=True,
-        lock=None,
-        fill_kwargs=True,
-        threadkey=None,
-        **kwargs,
+            callback_slot=None,
+            finished_slot=None,
+            except_slot=None,
+            default_exhandle=True,
+            lock=None,
+            fill_kwargs=True,
+            threadkey=None,
+            **kwargs,
     ):
         """
         Execute this workflow on the specified host. Connection will be a Connection object (WIP) keeping a connection
@@ -360,7 +380,7 @@ class Workflow(object):
         if observer in self._observers:
             self._observers.remove(observer)
 
-    def update(self):
+    def notify(self):
         for observer in self._observers:
             observer()
 
